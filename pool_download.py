987dfba09e4d1c5aa05202d4265c140d83ea17a5
itmorn <12567148@qq.com>
2022-07-05 14:19:09 +0800

    线程池版本

--- pool_download.py
+++ pool_download.py
@@ -1,16 +1,15 @@
-import random
-import time
 import requests
 import shutil
-from multiprocessing import Process, Manager
+from multiprocessing import Pool
 import os
-import math
+import traceback
 
 
 def get_lst_lines():
     lst = [
         f'https://dorc.ks3-cn-beijing.ksyun.com/data-set/2020Objects365%E6%95%B0%E6%8D%AE%E9%9B%86/val/zhiyuan_objv2_val.json',
-        f'https://dorc.ks3-cn-beijing.ksyun.com/data-set/2020Objects365%E6%95%B0%E6%8D%AE%E9%9B%86/train/zhiyuan_objv2_train.tar.gz']
+        f'https://dorc.ks3-cn-beijing.ksyun.com/data-set/2020Objects365%E6%95%B0%E6%8D%AE%E9%9B%86/train/zhiyuan_objv2_train.tar.gz'
+    ]
 
     # 验证集
     for i in range(44):
@@ -29,35 +28,19 @@ def get_lst_lines():
     return lst
 
 
-def download_file(url, filename):
-    while 1:
-        try:
-            torrent = requests.get(url, stream=True)
-            if torrent.status_code == 200:
-                with open(filename, 'wb') as f:
-                    for chunk in torrent.iter_content(1024):
-                        f.write(chunk)
-                break
-            else:
-                print(torrent.status_code, url)
-                time.sleep(1)
-        except Exception as e:
-            print(e, url)
-            time.sleep(1)
-
-
-def f(lst_lines):
-    for url in lst_lines:
-        print(url)
-
+def thread_task(url):
+    try:
         filename = os.path.basename(url)
         if "train" in url:
             filename = dir_train + filename
         else:
             filename = dir_val + filename
 
-        # download_file(url, filename)
         os.system(f"wget {url} -O {filename}")
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+
 
 if __name__ == '__main__':
     dir_train = "train/"
@@ -68,20 +51,15 @@ if __name__ == '__main__':
     os.makedirs(dir_val)
 
     lst_lines = get_lst_lines()
-    random.shuffle(lst_lines)
+    p = Pool(8)
+    try:
+        result_list = p.map(thread_task, lst_lines)
+        print(result_list)
 
-    manager = Manager()
-    mydict = manager.dict({"num": 0})
-    print(mydict)
-
-    core_num = 10
-    core_data_len = math.ceil(len(lst_lines) / core_num)
-
-    lst_p = []
-    for i in range(core_num):
-        p = Process(target=f,
-                    args=(lst_lines[i * core_data_len:i * core_data_len + core_data_len],))
-        p.start()
-        lst_p.append(p)
-    [p.join() for p in lst_p]
-    print(mydict)
+        p.close()
+        p.terminate()
+    except Exception as e:
+        print(e)
+    finally:
+        p.close()
+        p.terminate()
